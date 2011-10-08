9e18cf5b88e4cfd36188c737c0666e070204fbb6
Doug Stewart <doug@dougstewart.org>
2011-10-07 20:38:53 -0600

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watchthisnotthat::Application.config.secret_token = '74f77e0c759c1a3d9b5f6ede3875332cbf432397964d62c90c39c9b8aa1f4bafff1cdf477e52c8d35d090c9a8b83343cc909d6184a9e892cad3a85fdc3a6a539'

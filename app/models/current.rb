9823370dc0d57dab4ea98c5e4db35e8c77c7a0ed
Doug Stewart <doug@dougstewart.org>
2012-05-20 00:55:07 -0600

    Remove the arbitrary five-film limit, this way if there are a ton of good ones they can show up on the homepage. It's unlikely to happen, though.

--- app/models/current.rb
+++ app/models/current.rb
@@ -40,6 +40,8 @@ class Current < ActiveRecord::Base
   def self.sort_order
     # take the list, sort it by critic's score, get the top five items,
     # then reverse it so it's in descending order
-    @list = @list.sort_by{|critics_score| critics_score*+1}.pop(5).reverse
+    @list = @list.sort_by{|critics_score| critics_score*+1}.reverse
+    # artificially limit it to five films:
+    # @list = @list.sort_by{|critics_score| critics_score*+1}.pop(5).reverse
   end
 end
